06301019d6120a329b279996bae630a660e97f68
Elizabeth Frederickson <elizabeth@getlumn.com>
2026-02-23 14:16:04 -0700

    Fixes to file pathways and bumped version number

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: LUMN Utilites
 Plugin URI: https://getlumn.com
 Description: A set of custom shortcodes and tools for LUMN sites.
-Version: 3.0.0
+Version: 3.0.1
 Author: LUMN
 Author URI: https://getlumn.com
 License: GPL2
